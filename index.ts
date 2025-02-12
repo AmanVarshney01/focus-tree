84c25147cc103cae85dfd1730ff4653585a61656
Aman Varshney <amanvarshney.work@gmail.com>
2025-02-12 15:25:43 +0530

    update

--- index.ts
+++ index.ts
@@ -1,6 +1,39 @@
 #!/usr/bin/env node
 import { run, command, positional } from "@drizzle-team/brocli";
 
+const tree = [
+  '    *    ',
+  '   ***   ',
+  '  *****  ',
+  ' ******* ',
+  '*********',
+  '    |    ',
+  '    |    '
+];
+
+function growTree() {
+  const treeHeight = tree.length;
+  let revealedLines = 0;
+
+  const interval = setInterval(() => {
+    if (revealedLines < treeHeight) {
+      console.clear();
+      for (let i = 0; i < treeHeight; i++) {
+        if (i < treeHeight - revealedLines) {
+          console.log(' '.repeat(tree[i]?.length ?? 0));
+        } else {
+          console.log(tree[i]);
+        }
+      }
+      revealedLines++;
+    } else {
+      clearInterval(interval);
+      console.log('Tree fully revealed!');
+    }
+  }, 1000);
+}
+
+
 const echo = command({
   name: "echo",
   options: {
@@ -8,6 +41,8 @@ const echo = command({
   },
   handler: (opts) => {
     console.log(opts.text);
+  
+    growTree();
   },
 });
 
